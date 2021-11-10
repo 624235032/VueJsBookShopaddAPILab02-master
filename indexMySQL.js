e6ae91352e82c4675d91a3ddb43996fa64ef36e2
LAPTOP-20Q48UMK\User <624235032@parichat.skru.ac.th>
2021-11-10 20:39:50 +0700

    add env add student addbooks

--- indexMySQL.js
+++ indexMySQL.js
@@ -1,33 +1,145 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var fileUpload = require('express-fileupload');
+
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 var apiversion='/api/v1';
+
+var bookpicturepath=process.env.IMAGE_PATH;
+const secretkey =process.env.SECRET
 
 
 //MYSQL Connection
 var db = require('./config/db.config');
 
+const bcrypt = require('bcryptjs');
+
+const {sign,verify}  = require('./middleware.js');
 
 var port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(fileUpload());
+
 //Get all books
-app.get(apiversion + '/books',  function (req, res)  {  
+app.get(apiversion + '/books', verify, function (req, res)  { 
+  
+  try
+  {
+
+          res.setHeader('Content-Type', 'application/json');
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          
+          db.query('SELECT * FROM books', function (error, results, fields) {
+              if (error) throw error;
+              return res.send({ error: false, message: 'books list', data: results });
+          });
+
+          
+        }catch{
+          return res.status(401).send()
+        }
+
+        
+});
+
+//API ลงทะเบียนเพื่อรับ Token
+app.post(apiversion + '/auth/register', (req, res) => {
+
+  const hashedPassword = bcrypt.hashSync(req.body.password,10);
+
+  let user={
+      username: req.body.username,
+      role: req.body.role,
+      password: hashedPassword,
+  }
 
   res.setHeader('Content-Type', 'application/json');
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
-  db.query('SELECT * FROM books', function (error, results, fields) {
-      if (error) throw error;
-      return res.send({ error: false, message: 'books list', data: results });
+
+
+  try {
+
+    db.query(`INSERT INTO users 
+      (username,password,role) 
+      VALUES ( '${user.username}','${hashedPassword}','${user.role}');`,function (error, results, fields) {
+        if (error) throw error;
+        return res.status(201).send({ error: false, message: 'created a user' })  
+    });
+
+ }
+ catch(err) 
+ {
+
+   return res.send(err)
+   
+ }
+
+});
+
+//API ลงชื่อเข้าใช้งาน
+app.post(apiversion + '/auth/signin', (req, res) => {
+
+  db.query('SELECT * FROM users where username=?',req.body.username, function (error, results, fields) {
+
+    try
+    {
+      if (error) {
+
+        throw error;
+
+      }else{
+
+      
+        let hashedPassword=results[0].password
+        const correct =bcrypt.compareSync(req.body.password, hashedPassword)
+
+        if (correct)
+        {
+          let user={
+            username: req.body.username,
+            role: results.role,
+            password: hashedPassword,
+          }
+
+          // create a token
+          let token = sign(user, secretkey);
+          
+          res.setHeader('Content-Type', 'application/json');
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+          return res.status(201).send({ error: false, message: 'user sigin', accessToken: token });
+
+        }else {
+
+          return res.status(401).send("login fail")
+
+        }
+
+      }
+
+    }
+    catch(e)
+    {
+      return res.status(401).send("login fail")
+    }
+    
   });
 
   
+  
+
 });
+
 
 //Get all student
 app.get(apiversion + '/student',  function (req, res)  {  
@@ -121,6 +233,29 @@ app.delete(apiversion + '/student/:studentId',  function (req, res)  {
 
 
 
+app.post(apiversion + '/upload', (req, res) => {
+ 
+  if (!req.files) {
+      return res.status(500).send({ msg: "file is not found" })
+  }
+
+  const myFile = req.files.file;
+ 
+  myFile.mv(`${bookpicturepath}${myFile.name}`, function (err) {
+   
+      if (err) {
+          console.log(err)
+          return res.status(500).send({ msg: "Error occured" });
+      }
+     
+      return res.send({name: myFile.name, path: `/${myFile.name}`});
+
+  });
+
+});
+
+
+
 //Add new book
 app.post(apiversion + '/book',  function (req, res)  {  
 
@@ -153,6 +288,7 @@ app.post(apiversion + '/book',  function (req, res)  {
 
 
 });
+
 
 
 //Add new student
@@ -241,6 +377,9 @@ app.put(apiversion + '/student/:studentId',  function (req, res)  {
    });
 
 });
+
+
+
 
 
 app.listen(port, function () {
